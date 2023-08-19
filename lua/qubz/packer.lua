ff8072f7a505a8ada6e62408af2061175678c940
Juhani Kupiainen <juhani@jkupiainen.fi>
2023-08-19 19:07:45 +0300

    Clean out empty files

--- lua/qubz/packer.lua
+++ lua/qubz/packer.lua
@@ -1,43 +1,43 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		-- or                              , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        -- or                              , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-	use ({
-		'Mofiqul/dracula.nvim',
-		as = 'dracula',
-		--config = function()
-		--	vim.cmd('colorscheme dracula')
-		--end
-	})
+    use({
+        'Mofiqul/dracula.nvim',
+        as = 'dracula',
+        --config = function()
+        --	vim.cmd('colorscheme dracula')
+        --end
+    })
 
-	use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
---	use ('nvim-treesitter/playground')
-	use ('theprimeagen/harpoon')
-	use ('mbbill/undotree')
-	use ('tpope/vim-fugitive')
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    --	use ('nvim-treesitter/playground')
+    use('theprimeagen/harpoon')
+    use('mbbill/undotree')
+    use('tpope/vim-fugitive')
 
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'L3MON4D3/LuaSnip'},
-		}
-	}
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'L3MON4D3/LuaSnip' },
+        }
+    }
 
-	use 'chrisgrieser/nvim-spider'
+    use 'chrisgrieser/nvim-spider'
 end)
